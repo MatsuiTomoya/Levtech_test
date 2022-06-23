40bbef873ed663a9657068b2c72ac19d67bdde69
MatsuiTomoya <ibl53985398@gmail.com>
2022-06-23 16:21:19 +0900

    [add]記事一覧画面

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
